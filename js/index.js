a5208719923f8276a42f1644584f95946f23dfc2
cyan596 <cyan.zhukeqing@qq.com>
2018-05-26 16:15:44 +0800

    8
    8

--- js/index.js
+++ js/index.js
@@ -45,11 +45,11 @@
         }
         // timerP2.innerHTML= surplusDays+"天"+surplusHours+"小时"+surplusMinutes+"分钟"+(surplusSeconds+1)+"秒"+"</p>";
         var tChildArr=timerP2.childNodes;
-        tChildArr[0].innerHTML=surplusDays+"天";
-        tChildArr[1].innerHTML=surplusHours+"小时";
-        tChildArr[2].innerHTML=surplusMinutes+"分钟";
+        tChildArr[0].innerHTML=(surplusDays+1)+"天";
+        tChildArr[1].innerHTML=(surplusHours+1)+"小时";
+        tChildArr[2].innerHTML=(surplusMinutes+1)+"分钟";
         tChildArr[3].innerHTML=(surplusSeconds+1)+"秒";
-        console.log(tChildArr);
+        // console.log(tChildArr);
 
         // console.log(1);
     }
@@ -72,3 +72,50 @@
         '请问阁下是叫有容吗？'
     ];
     
+
+    //设置记录页面宽高
+    var h=window.innerHeight||document.body.clientHeight||document.documentElement.clientHeight;
+    var w=window.innerWidth||document.body.clientWidth||document.documentElement.clientWidth;
+    console.log(w);
+    $(window).on("load",function(){
+           
+
+            // console.log(h);
+            $("#r-container").css("height",h);
+            $("#r-container").css("width",w);
+        });
+
+    // 滑动事件处理
+    var container = document.getElementsByClassName("p-container")[0];
+    // console.log(pMain);
+    var mc = new Hammer.Manager(container);
+    Swipe = new Hammer.Swipe();
+    mc.add(Swipe);
+    mc.on('swipeleft', function (ev) {
+        if(ev.deltaX>4){
+
+        }
+         $(".p-container").css('transform','translateX('+(-w)+'px)');
+             console.log("向左移动ss");
+        console.log("向左移动");
+    });
+    mc.on('swiperight', function (ev) {
+        if(ev.deltaX>4){
+           
+        }
+         $(".p-container").css('transform','translateX('+(0)+'px)');
+        console.log("向右移动");
+    });
+
+    // hammerstart.get('pan').set({
+    //   direction: Hammer.DIRECTION_LEFT
+    // });
+    // hammerstart.on('pan', function(ev) {
+    //     if(ev.deltaX>10){
+    //         $(".p-container").css('transform','translateX(-100%)');
+    //     }
+    // });
+    // var pHammer = new Hammer(myElement, myOptions);
+    //     pHammer.on('pan', function(ev) {
+    //     console.log(ev); //输出拖移事件对象
+    // });
